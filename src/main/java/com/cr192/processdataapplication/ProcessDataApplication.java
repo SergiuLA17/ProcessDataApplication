5ac1684738fcd930f142cc78064b8ce02bba9275
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-10-15 08:46:12 +0300

    project creation

---
+++ src/main/java/com/cr192/processdataapplication/ProcessDataApplication.java
@@ -0,0 +1,13 @@
+package com.cr192.processdataapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessDataApplication.class, args);
+    }
+
+}
